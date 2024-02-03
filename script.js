978c152ed298dd52dba0ed281748a64d922e7352
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2024-02-03 20:59:32 +0530

    edit is working

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const sub=document.querySelector(".post-btn");
 const right=document.querySelector(".right-main");
 const main_input=document.querySelector(".input-comment");
+const textcontent = document.querySelector("#text-area");
+
+textcontent.addEventListener("input",(e)=>{
+var length = e.target.value.length;
+document.getElementById("post_count").innerHTML=`${length}/100`;
+})
+
 sub.addEventListener('click', () => {
     var inside = main_input.value;
     main_input.value="";
@@ -12,7 +19,7 @@ sub.addEventListener('click', () => {
                     <div class="user-1">
                         <p class="username">@mainsh</p>
                         <div class="edit">
-                            <button  class="transparent-button" id="edit">
+                            <button  class="transparent-button edit-btn"  >
                                 <img  src="https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/028/original/edit.png?1706888661">
                             </button>
                             <button  class="transparent-button delete">
@@ -33,6 +40,25 @@ sub.addEventListener('click', () => {
     `;
 
     const del=document.querySelectorAll(".delete");
+    const r_input = document.querySelectorAll(".right-input");
+    
+
+r_input.forEach(input => {
+    input.setAttribute('readonly', true);
+});
+const editButtons = document.querySelectorAll(".edit-btn");
+editButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        const textarea = e.target.parentNode.parentNode.parentNode.parentNode.querySelector(".right-input");
+        console.log("manish")
+        textarea.removeAttribute('readonly');
+        textarea.focus()
+
+        textarea.addEventListener("focusout", () => {
+            textarea.setAttribute('readonly', true);
+        });
+    });
+});
     del.forEach(ele=>{
         ele.addEventListener("click",(e)=>{
             e.target.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
